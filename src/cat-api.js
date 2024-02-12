ffcdfc53a22b7dc616aeda46a83e3b8cd56038de
Olena-Horobets <80359219+Olena-Horobets@users.noreply.github.com>
2024-02-12 18:48:07 -0330

    cat info render added

--- src/cat-api.js
+++ src/cat-api.js
@@ -5,3 +5,14 @@ export function fetchBreeds(params) {
     return res.json();
   });
 }
+
+export function fetchCatByBreed(breedId, params) {
+  return fetch(
+    `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`,
+    params
+  ).then(res => {
+    if (!res.ok) throw new Error(response.statusText);
+
+    return res.json();
+  });
+}
--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@
 
 // axios.defaults.headers.common['x-api-key'] =
 //   'live_o0DjDhD9sQJ8vSBYerclRajz3qjShq8uQvqFj4h0k2iq6Aj7uknNu1uxnQrOZVNB';
-import { fetchBreeds } from './cat-api';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
 const refs = {
-  list: document.querySelector('.breed-select'),
+  select: document.querySelector('.breed-select'),
   loader: document.querySelector('.loader'),
   error: document.querySelector('.error'),
   catInfo: document.querySelector('.cat-info'),
@@ -21,8 +21,43 @@ function createSelectMarkup(arr) {
   return arr.map(el => `<option value="${el.id}">${el.name}</option>`).join('');
 }
 
+function createMainMarkup({
+  name,
+  weight: { imperial },
+  intelligence,
+  description,
+  url,
+}) {
+  return `<div class="card">
+            <h2>Name: ${name}</h2>
+            <p>Weight: ${imperial} kg</p>
+            <p>Intelligence: ${intelligence}</p>
+            <p>${description}</p>
+            <img src="${url}" width=400 />
+        </div>`;
+}
+
+function onSelectChange(e) {
+  const value = e.currentTarget.value;
+  fetchCatByBreed(value, params)
+    .then(res => {
+      const cat = JSON.parse(localStorage.getItem('breeds')).find(
+        el => el.id === value
+      );
+
+      refs.catInfo.innerHTML = createMainMarkup({
+        ...cat,
+        url: res[0].url,
+      });
+    })
+    .catch(error => console.log('error', error));
+}
+
 fetchBreeds(params)
   .then(res => {
-    refs.list.innerHTML = createSelectMarkup(res);
+    localStorage.setItem('breeds', JSON.stringify(res));
+    refs.select.innerHTML = createSelectMarkup(res);
   })
   .catch(error => console.log('error', error));
+
+refs.select.addEventListener('change', onSelectChange);
